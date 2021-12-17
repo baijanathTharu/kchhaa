be64bbcab1eb30ab50cb7cc86aea8f9eafd8df04
baijanathTharu <bnchaudhary.tharu@gmail.com>
2021-12-17 19:17:26 +0545

    style posts

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -5,7 +5,7 @@ export const Posts = () => {
         return (
           <div
             key={i}
-            className='mx-auto overflow-hidden bg-white rounded-lg shadow-lg'
+            className='mx-auto my-3 overflow-hidden bg-primary hover:bg-primary-focus text-primary-content rounded-lg shadow-lg'
           >
             <div className='p-6'>
               <div>
@@ -17,26 +17,22 @@ export const Posts = () => {
                         src='https://images.unsplash.com/photo-1586287011575-a23134f797f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=48&q=60'
                         alt='Avatar'
                       />
-                      <a href='#' className='mx-2 font-semibold text-gray-700'>
+                      <a href='#' className='mx-2 font-semibold'>
                         Jone Doe
                       </a>
                     </div>
-                    <span className='mx-1 text-xs text-gray-600'>
-                      21 SEP 2015
-                    </span>
+                    <span className='mx-1 text-xs'>21 SEP 2015</span>
                   </div>
                 </div>
 
-                <span className='text-xs font-medium text-blue-600 uppercase'>
-                  Education
-                </span>
+                <span className='text-xs font-medium'>Education</span>
                 <a
                   href='#'
-                  className='block mt-2 text-2xl font-semibold text-gray-800  hover:text-gray-600 hover:underline'
+                  className='block mt-2 text-2xl font-semibold  hover hover:underline'
                 >
                   I Built A Successful Blog In One Year
                 </a>
-                <p className='mt-2 text-sm text-gray-600'>
+                <p className='mt-2 text-sm'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Molestie parturient et sem ipsum volutpat vel. Natoque sem et
                   aliquam mauris egestas quam volutpat viverra. In pretium nec
